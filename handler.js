9ee679ba702e14f7dec2ca29fef8fd3791b06a35
Welington Cruz de Assis <welingtoncassis@gmail.com>
2020-06-18 22:00:42 -0300

    feat(handler): add detectImages function and require {fs}

--- handler.js
+++ handler.js
@@ -1,12 +1,35 @@
 'use strict';
 
+const { promises: {readFile} }= require('fs') // elimina o callback
+
 class Handler {
+
   constructor({rekoSvc}) {
     this.rekoSvc = rekoSvc
   }
-  
+
+  async detectImagesLabels(buffer) {
+    const result = await this.rekoSvc.detectLabels({
+      Image: {
+        Bytes: buffer
+      }
+    }).promise()
+
+    const workingItems = result.Labels
+      .filter(({Confidence})=> Confidence > 80);
+    
+    const names = workingItems
+      .map(({Name}) => Name)
+      .join(' and ')
+    
+    return { names, workingItems }
+  }
+
   async main(event) {
     try {
+      const imgBuffer = await readFile('./images/download.jpeg')
+      const { names, workingItems } = await this.detectImagesLabels(imgBuffer)
+      console.log(names, workingItems)
       return {
         statusCode: 200,
         body: 'Hello!'
@@ -19,6 +42,7 @@ class Handler {
       }
     }
   }
+
 }
 
 const aws = require('aws-sdk')
@@ -26,6 +50,7 @@ const reko = new aws.Rekognition()
 
 //factory
 const handler = new Handler({
-  rekoSvc = reko
+  rekoSvc : reko
 })
-module.exports.main = handler.main.bind('h')
+
+module.exports.main = handler.main.bind(handler)
